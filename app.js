8eb76201d970097e1998498b6f8b2cff427acb48
janellegage <jgagerob@asu.edu>
2023-07-19 11:07:44 -0400

    Initial

---
+++ app.js
@@ -0,0 +1,25 @@
+const value =  document.querySelector(".value")
+const buttons =  document.querySelectorAll(".btn")
+
+let num = 0
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const styles = event.currentTarget.classList
+        if (styles.contains("btn-outline-danger")){
+            num--
+        } else if (styles.contains("btn-outline-success")){
+            num++
+        } else {
+            num = 0
+        }
+        value.textContent = num
+        if (num > 0){
+            value.style.color = "green"
+        } else if (num < 0 ){
+            value.style.color = "red"
+        } else {
+            value.style.color = "black"
+        }
+    })
+})
